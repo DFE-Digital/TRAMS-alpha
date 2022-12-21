60be18464aad74cdd58e478d894267115aca21ca
jenhadfield-dfe <jen.hadfield@education.gov.uk>
2022-12-21 13:11:24 +0000

    set up

--- app/routes.js
+++ app/routes.js
@@ -1,9 +1,37 @@
-// 
-// For guidance on how to create routes see:
-// https://prototype-kit.service.gov.uk/docs/create-routes
-// 
+const express = require('express')
+const router = express.Router()
 
-const govukPrototypeKit = require('govuk-prototype-kit')
-const router = govukPrototypeKit.requests.setupRouter()
+var NotifyClient = require('notifications-node-client').NotifyClient
 
-// Add your routes here
+var notifyClient = new NotifyClient(process.env.apikey)
+
+// Add your routes here - above the module.exports line
+
+router.get('/academy/financial', function (req, res) {
+  req.session.data = {}
+
+  return res.render('academy/financial', {})
+})
+
+router.get('/reporting/fha/cya', function (req, res) {
+  req.session.data['cya'] = 'true'
+
+  return res.render('reporting/fha/cya', {})
+})
+
+router.get('/reporting/fha/complete', function (req, res) {
+  if (process.env.send === 'true') {
+    notifyClient
+      .sendEmail(process.env.template, process.env.email, {
+        personalisation: {},
+      })
+      .then((response) => console.log(response))
+      .catch((err) => console.error(err))
+  }
+
+
+
+  return res.render('reporting/fha/complete', {})
+})
+
+module.exports = router
