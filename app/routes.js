5ab46f0b1ef9b48cc439874dfc713be8cf10b6f0
Sara Gowen <9001998+dynamictulip@users.noreply.github.com>
2023-03-24 16:57:04 +0000

    Added search routing and variables

--- app/routes.js
+++ app/routes.js
@@ -2,8 +2,23 @@
 // For guidance on how to create routes see:
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
+let trusts = require('./data/invented-trust-data').trusts;
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+router.post('/version-1/trust-details', function (request, response) {
+  let searchterm = request.body.search.toLowerCase();
+
+  let trust = trusts.filter(t => t.Name.toLowerCase().includes(searchterm) || t.Uid === searchterm)[0];
+
+  if (searchterm && trust) {
+    response.locals.trust = trust;
+    response.render('version-1/trust-details');
+  }
+  else {
+    response.locals.searchterm = searchterm;
+    response.locals.trusts = trusts.slice(0, 10);
+    response.render('version-1/not-found');
+  }
+});
