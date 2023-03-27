fc4088a6a72a01aff0cde94696b70f2a44fdd2e4
Liz Daly <liz7daly@gmail.com>
2023-03-27 16:32:18 +0100

    Update variables to be under Version-2
    - Move not found page to version 2 section
    - update router to version 2
    - refactor JavaScript to use const

--- app/routes.js
+++ app/routes.js
@@ -2,23 +2,25 @@
 // For guidance on how to create routes see:
 // https://prototype-kit.service.gov.uk/docs/create-routes
 //
-let trusts = require('./data/invented-trust-data').trusts;
+const trusts = require('./data/invented-trust-data').trusts;
 
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-router.post('/version-1/trust-details', function (request, response) {
-  let searchterm = request.body.search.toLowerCase();
-
-  let trust = trusts.filter(t => t.Name.toLowerCase().includes(searchterm) || t.Uid === searchterm)[0];
-
-  if (searchterm && trust) {
-    response.locals.trust = trust;
-    response.render('version-1/trust-details');
+router.post('/version-2/trust-details', function (request, response) {
+  let trust;
+  const searchTerm = request.session.data.search;
+  if (searchTerm) {
+    const searchTermLower = searchTerm.toLowerCase();
+    trust = trusts.filter(t => t.Name.toLowerCase().includes(searchTermLower) || t.Uid === searchTermLower)[0];
   }
-  else {
-    response.locals.searchterm = searchterm;
+
+  if (trust) {
+  response.locals.trust = trust;
+  response.render('version-2/trust-details');
+  } else {
+    response.locals.searchTerm = searchTerm;
     response.locals.trusts = trusts.slice(0, 10);
-    response.render('version-1/not-found');
+    response.redirect('./not-found');
   }
 });
