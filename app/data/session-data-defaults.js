c58ca527fef0e81bf901ad56bb6eb7b3a6f2b463
Liz Daly <liz7daly@gmail.com>
2023-03-29 14:49:45 +0100

    Update generated data
    Now includes values for all fields

--- app/data/session-data-defaults.js
+++ app/data/session-data-defaults.js
@@ -1,6 +1,44 @@
 module.exports = {
   trust: {
-    name: 'Abbey Lane Academies Trust',
-    uid: '1234',
+    "name": "Abbey Lane Academies Trust",
+    "uid": "9490",
+    "trustType": "Multi-academy trust",
+    "trustDetails": {
+      "trustRelationshipManager": {
+        "name": "Maribel Ferry",
+        "email": "Maribel.Ferry@education.gov.uk",
+        "telephone": "016977 5395"
+      },
+      "sfsoLead": {
+        "name": "Felipa Durgan",
+        "email": "Felipa.Durgan@education.gov.uk",
+        "telephone": "028 7429 2085"
+      },
+      "mainContactAtTrust": {
+        "name": "Gerard Heathcote",
+        "email": "Gerard.Heathcote@abbeylaneacademiestrust.co.uk",
+        "telephone": "014274 27945"
+      },
+      "address": "Altenwerth Streets, Rotherham, KL4 1ID",
+      "website": "https://www.abbeylaneacademiestrust.co.uk",
+      "localAuthorities": [
+        "Sheffield"
+      ],
+      "regionsGroup": "Yorkshire and the Humber",
+      "schoolsFinancialSupportTerritory": "Yorkshire and the Humber",
+      "dateIncorporated": "0001-01-01T00:00:00",
+      "dateIncorporatedString": "17 Oct 2019",
+      "dateOpened": "0001-01-01T00:00:00",
+      "dateOpenedString": "10 Aug 2020",
+      "trustReferenceNumber": "TR5401",
+      "companiesHouseNumber": "03995823",
+      "companiesHouseFilingHistoryUrl": "https://find-and-update.company-information.service.gov.uk/company/03995823/filing-history",
+      "getInformationAboutSchoolsUrl": null,
+      "sponsorStatus": "Approved",
+      "sponsorRestrictions": "No restrictions",
+      "sponsorApprovalDate": "0001-01-01T00:00:00",
+      "sponsorApprovalDateString": "30 Mar 2021",
+      "sponsorName": "Illuminate Multi-Academy Trust"
+    }
   }
 }
