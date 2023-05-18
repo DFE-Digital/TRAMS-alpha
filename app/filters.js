cc8e94a54aeec571ebd4fa00aaf87432e9b8e277
Liz Daly <liz7daly@gmail.com>
2023-05-18 14:55:02 +0100

    Add local authority count

--- app/filters.js
+++ app/filters.js
@@ -1,6 +1,7 @@
 const govukPrototypeKit = require('govuk-prototype-kit');
 const { formatAcademyRows } = require('./assets/javascripts/academiesUtils');
 const { formatGovernorRows } = require('./assets/javascripts/governanceUtils');
+const { getLocalAuthoritiesWithCount } = require('./assets/javascripts/trust-utils');
 const addFilter = govukPrototypeKit.views.addFilter
 
 addFilter("formatDate", function (date) {
@@ -25,3 +26,4 @@ addFilter("formatNumber", function (num) {
 
 addFilter("formatAcademyRows", formatAcademyRows);
 addFilter("formatGovernorRows", formatGovernorRows);
+addFilter("getLocalAuthoritiesWithCount", getLocalAuthoritiesWithCount);
