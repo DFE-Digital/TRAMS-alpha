8721daccbe2efee1111ca2a693b4955f5ffa5b57
Liz Daly <liz7daly@gmail.com>
2023-05-19 09:20:54 +0100

    Add missing end lines

--- app/filters.js
+++ app/filters.js
@@ -1,28 +1,29 @@
-const govukPrototypeKit = require('govuk-prototype-kit');
-const { formatAcademyRows } = require('./assets/javascripts/academiesUtils');
-const { formatGovernorRows } = require('./assets/javascripts/governanceUtils');
-const { getLocalAuthoritiesWithCount } = require('./assets/javascripts/trust-utils');
-const addFilter = govukPrototypeKit.views.addFilter
+const govukPrototypeKit = require("govuk-prototype-kit");
+const { formatAcademyRows } = require("./assets/javascripts/academiesUtils");
+const { formatGovernorRows } = require("./assets/javascripts/governanceUtils");
+const {
+  getLocalAuthoritiesWithCount,
+} = require("./assets/javascripts/trust-utils");
+const addFilter = govukPrototypeKit.views.addFilter;
 
 addFilter("formatDate", function (date) {
-    try {
-        const newDate = new Date(date);
+  try {
+    const newDate = new Date(date);
 
-        let options = {
-            year: "numeric",
-            month: "short",
-            day: "numeric",
-        };
-        return new Intl.DateTimeFormat("en-GB", options).format(newDate);
-    }
-    catch {
-        return "---"
-    }
+    let options = {
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+    };
+    return new Intl.DateTimeFormat("en-GB", options).format(newDate);
+  } catch {
+    return "---";
+  }
 });
 
 addFilter("formatNumber", function (num) {
-    return num.toLocaleString(); 
-})
+  return num.toLocaleString();
+});
 
 addFilter("formatAcademyRows", formatAcademyRows);
 addFilter("formatGovernorRows", formatGovernorRows);
